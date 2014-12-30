4872e5536912123a392ea6741bd21dc4a4301391
koseki <dainaso97@gmailcom>
2014-12-31 00:09:23 +0900

    Initial Commit

---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by 前田 晃良 on 2014/12/31.
+//  Copyright (c) 2014年 A.M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSSReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSReader
+//
+//  Created by 前田 晃良 on 2014/12/31.
+//  Copyright (c) 2014年 A.M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
